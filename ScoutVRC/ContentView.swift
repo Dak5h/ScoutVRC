1951b565f80edd48d3cb3398be04cd73519196d2
Daksh Gupta <daksh.gupta712@gmail.com>
2024-06-14 13:38:02 -0700

    Initial Commit

---
+++ ScoutVRC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScoutVRC
+//
+//  Created by Daksh Gupta on 6/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScoutVRC/ScoutVRCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutVRCApp.swift
+//  ScoutVRC
+//
+//  Created by Daksh Gupta on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ScoutVRCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
